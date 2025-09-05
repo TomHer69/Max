f989bdb7cb063b9fedada8fff91673f600eb75aa
thher <th.herzog@web.de>
2025-09-05 15:10:07 +0200

    Initialer Commit: EJS-Projekt mit zwei Bildseiten

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('seite1');
+});
+
+app.get('/seite2', (req, res) => {
+  res.render('seite2');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
